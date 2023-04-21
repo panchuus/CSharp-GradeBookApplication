ff7d2f2dbfab2d116e2122cad3ecf8d0a121b700
panchuus <panchamikamath1999@gmail.com>
2023-04-21 12:08:43 +0530

    2

---
+++ GradeBook/GradeBooks/RankedGradeBook.cs
@@ -0,0 +1,14 @@
+
+
+using System;
+
+namespace GradeBook.GradeBooks
+{
+    public class RankedGradeBook : BaseGradeBook
+    {
+        public RankedGradeBook(string name) : base(name)
+        {
+            Type = Enums.GradeBookType.Ranked;
+        }
+    }
+}
