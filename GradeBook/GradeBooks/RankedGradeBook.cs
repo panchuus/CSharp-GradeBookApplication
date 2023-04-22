954ee6cbe0e8ce25dcbbebd67c2e8791e2bfc0bf
panchuus <panchamikamath1999@gmail.com>
2023-04-22 12:49:35 +0530

    module 2:1 changes

--- GradeBook/GradeBooks/RankedGradeBook.cs
+++ GradeBook/GradeBooks/RankedGradeBook.cs
@@ -12,14 +12,7 @@ namespace GradeBook.GradeBooks
         }
     }
 
-    public override char GetLetterGrade(double averageGrade)
-    {
-        if (Students.Count < 5)
-        {
-            throw new InvalidOperationException("Ranked-grading requires a minimum of 5 students to work");
-        }
-        return 'F';
-    }
+   
 
 
 
