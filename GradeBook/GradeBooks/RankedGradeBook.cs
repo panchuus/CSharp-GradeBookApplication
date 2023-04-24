7a85f5e7acf820199f155684088368c4a18b30f5
panchuus <panchamikamath1999@gmail.com>
2023-04-24 22:03:23 +0530

    module 3

--- GradeBook/GradeBooks/RankedGradeBook.cs
+++ GradeBook/GradeBooks/RankedGradeBook.cs
@@ -36,7 +36,16 @@ namespace GradeBook.GradeBooks
             return 'F';
         }
 
+        public override void CalculateStatistics()
+        {
+            if (Students.Count < 5)
+            {
+                Console.WriteLine("Ranked grading requires at least 5 students.");
+                return;
+            }
 
+            base.CalculateStatistics();
+        }
 
 
     }
